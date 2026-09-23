a0d7cee11500b1872f18de9c41f590fe9ecfd527
soysajee47 <soysajee.j@psru.ac.th>
2026-09-23 22:02:41 +0700

    Add files via upload

---
+++ ConnDB.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "altaria.proxy.rlwy.net";
+$username = "root";
+$password = "VujIQdtUhxQFggLnchLxmiHPeHmsqlRI";
+$dbname = "db_cpe039";
+$port = "55792";
+
+try {
+
+  $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8mb4", $username, $password);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+} catch(PDOException $e) {
+  throw $e;
+}
+?>
